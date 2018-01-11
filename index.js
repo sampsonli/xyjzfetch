cd0006fa61cf7ef67de104e4fb498d23da89dca5
李春 <lichun@500wan.com>
2018-01-11 17:31:07 +0800

    fixbug

--- index.js
+++ index.js
@@ -6,48 +6,56 @@ import baseinfo from './app/models/baseinfo';
 
 
 
-async function getOnePage (pn) {
+async function getOnePage(pn) {
   const list = await getCompAndCidList(pn);
-    if(list.length) {
-      let companyList = [], company;
-      for (let i = 0; i < list.length; i++) {
-        company = {}
-        company.name = list[i].name
-        company.zizhi = await getZiZhiList(list[i].guid)
-        company.plist = await getPersonList(list[i].guid)
-        companyList.push(company)
-      }
-  
-      for (let i = 0; i < companyList.length; i++) {
-        let base = await baseinfo.findOne({name: companyList[i].name})
-        let allinfo = companyList[i]
-        if(base) {
-          allinfo = {...base, ...companyList[i]}
-        }
+  if (list.length) {
+    let companyList = [], company;
+    for (let i = 0; i < list.length; i++) {
+      company = {}
+      company.name = list[i].name
+      company.zizhi = await getZiZhiList(list[i].guid)
+      company.plist = await getPersonList(list[i].guid)
+      companyList.push(company)
+    }
+
+    for (let i = 0; i < companyList.length; i++) {
+      let base = await baseinfo.findOne({ name: companyList[i].name })
+      let allinfo = companyList[i]
+      if (base) {
+        allinfo = { ...base, ...companyList[i] }
         await Company.findOneAndUpdate({ name: companyList[i].name }, allinfo, { upsert: true })
       }
-      const total = await Company.count()
-      console.log(`第${pn}次抓取， 当前一共有${total}条数据`)
+      // await Company.findOneAndUpdate({ name: companyList[i].name }, allinfo, { upsert: true })
     }
+    const total = await Company.count()
+    console.log(`第${pn}次抓取， 当前一共有${total}条数据`)
+  }
 }
 (async () => {
 
   const connect = await connectDatabase('mongodb://localhost/xyjzfetch-test')
-  // for (let i = 0; i < 365; i++) {
-  //   try {
-  //     await getOnePage(i + 1)
-  //   } catch (e) {
-  //     console.log(`第${i+1}次抓取失败， 失败原因：${e.message}`)
-  //   }
-  // }
-  // 
-  let list = getBasicInfo('C:/Users/sampson/Desktop/fjszsgdw/show')
-  console.log(list)
-  for(let i = 0; i < list.length; i++) {
-    await baseInfo.findOneAndUpdate({ name: list[i].name }, list[i], { upsert: true })
+  try {
+    if (process.env.CRAW_WEB === 'true') {
+      for (let i = 0; i < 365; i++) {
+        try {
+          await getOnePage(i + 1)
+        } catch (e) {
+          console.log(`第${i + 1}次抓取失败， 失败原因：${e.message}`)
+        }
+      }
+  
+    } else {
+      let list = getBasicInfo('C:/Users/sampson/Desktop/fjszsgdw/show')
+      console.log(list)
+      for (let i = 0; i < list.length; i++) {
+        await baseInfo.findOneAndUpdate({ name: list[i].name }, list[i], { upsert: true })
+      }
+    }
+  } catch(e) {
+
+    console.error(e.message)
+  } finally {
+    connect.close()
   }
-
-
-
-  connect.close()
+  
 })()
